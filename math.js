21a16b51d42b6b2d3d6aa9375cb243d788a5b6d4
Xylian <33781686+XylianZeref@users.noreply.github.com>
2019-06-05 21:33:32 +0200

    simplify and improve milisecondsCalc()

--- math.js
+++ math.js
@@ -1,10 +1,9 @@
 function milisecondsCalc(years, months, days, hours, minutes, seconds, miliseconds) {
-	let ret = years*3.154e+10;
-	ret += months*2.628e+9;
-	ret += days*8.64e+7;
-	ret += hours*3.6e+6;
-	ret += minutes*60000;
-	ret += seconds*1000;
-	ret += miliseconds;
-	return ret;
+	return years*3.154e+10
+		+ months*2.628e+9
+		+ days*8.64e+7
+		+ hours*3.6e+6
+		+ minutes*60000
+		+ seconds*1000
+		+ miliseconds;
 }
